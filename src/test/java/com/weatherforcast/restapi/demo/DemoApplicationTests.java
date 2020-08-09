9953ecc0ce8a0ebeac1db9ae8b39d5d54e1f4f0d
Bharti <rbharti4@LAMU02X40K2JG5M.uhc.com>
2020-08-09 17:29:44 +0530

    modified junit

--- src/test/java/com/weatherforcast/restapi/demo/DemoApplicationTests.java
+++ src/test/java/com/weatherforcast/restapi/demo/DemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.weatherforcast.restapi.demo;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.weatherforcast.restapi.demo.api.WeatherApi;
+import com.weatherforcast.restapi.demo.entity.WeatherReport;
+import com.weatherforcast.restapi.demo.service.WeatherService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
+@Slf4j
 class DemoApplicationTests {
 
+	@Mock
+	RestTemplate restTemplate;
+	
+	@InjectMocks
+	WeatherApi weatherService = new WeatherService();
+	
+	@Value("${weather-url}")
+	private String weatherURL;
+	
 	@Test
 	void contextLoads() {
+		log.info("testWeatherReport()=========================");
+		when(restTemplate.getForEntity(weatherURL, WeatherReport.class)).thenReturn(new ResponseEntity<WeatherReport>(null));
+		assertNotNull(weatherService.getReport().getBody());
 	}
 
 }
